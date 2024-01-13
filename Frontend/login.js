649bbbb25b48f6fe25db96148fb0f8bbf7e2265c
reethuthota <reethu.thota@gmail.com>
2024-01-13 11:57:24 +0530

    Create frontend

---
+++ Frontend/login.js
@@ -0,0 +1,4 @@
+function redirectRecruiterLogin() {
+    // Redirect to the recruiter login page (replace 'recruiter-login.html' with your desired page)
+    window.location.href = 'recruiter-login.html';
+}
